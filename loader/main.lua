c48b328e6f6f9decedbbfcf5ffe95bf141d25ef2
Achitsak <81982587+Achitsak@users.noreply.github.com>
2025-07-13 21:41:53 +0700

    Create main.lua

---
+++ loader/main.lua
@@ -0,0 +1,18 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players.LocalPlayer
+
+local list = {
+  [35789249] = "grow_a_garden",
+  [5292947] = "all_star_tower_defense_x",
+  [4372130] = "bloxfruits"
+}
+
+local target = game.CreatorId
+local script = list[target]
+
+if scriptName then
+  local url = string.format("https://raw.githubusercontent.com/xshiba/MasterPClient/main/%s.lua", script)
+  loadstring(game:HttpGet(url))()
+else
+  print('not found scriptName!')
+end
