500ff95f8d9ddc4b02867177a0d805a8e1f0a3ed
Achitsak <81982587+Achitsak@users.noreply.github.com>
2025-07-13 21:41:08 +0700

    Create all_star_tower_defense_x.lua

---
+++ loader/scripts/all_star_tower_defense_x.lua
@@ -0,0 +1,3 @@
+print('this is loadstring for all_star_tower_defense_x')
+script_key="ILpvZXNXQVbTCAOuDDtOEhYblJLbIcEf";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2a15f4a97e3a10f1a10dbb55c265b1f0.lua"))()
