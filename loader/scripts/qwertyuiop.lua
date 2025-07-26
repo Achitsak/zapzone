ebb08abb1a7b4ec09fe7c5bf7719231d51511727
Achitsak <81982587+Achitsak@users.noreply.github.com>
2025-07-27 00:14:00 +0700

    Update qwertyuiop.lua

--- loader/scripts/qwertyuiop.lua
+++ loader/scripts/qwertyuiop.lua
@@ -279,3 +279,8 @@ task.spawn(function()
         end)
     end
 end)
+
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+end)
