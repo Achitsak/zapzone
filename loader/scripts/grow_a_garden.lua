f81a06d77f2cdc27e945bbd1aafd37d859675c1f
Achitsak <81982587+Achitsak@users.noreply.github.com>
2025-08-12 03:44:15 +0700

    Update grow_a_garden.lua

--- loader/scripts/grow_a_garden.lua
+++ loader/scripts/grow_a_garden.lua
@@ -6,7 +6,7 @@ until game:GetService("Players").LocalPlayer:GetAttribute('DataFullyLoaded') == 
 print('this is loadstring for grow a garden')
 
 getgenv().ConfigsKaitun = {
-	Beta_Fix_Data_Sync = false,
+	Beta_Fix_Data_Sync = true,
 
 	NoDeletePlayer = false,
 
