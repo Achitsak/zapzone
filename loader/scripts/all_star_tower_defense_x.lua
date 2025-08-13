8bcdc78fe43d3b0fa4f18e0e4b7bb9b62cf965bb
Achitsak <81982587+Achitsak@users.noreply.github.com>
2025-08-13 17:10:28 +0700

    Update all_star_tower_defense_x.lua

--- loader/scripts/all_star_tower_defense_x.lua
+++ loader/scripts/all_star_tower_defense_x.lua
@@ -1,3 +1,23 @@
 print('this is loadstring for all_star_tower_defense_x')
-script_key="ILpvZXNXQVbTCAOuDDtOEhYblJLbIcEf";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2a15f4a97e3a10f1a10dbb55c265b1f0.lua"))()
+repeat task.wait(10) until game:IsLoaded()
+getgenv().Script_Mode = "Kaitun_Script"
+_G.Settings_Farm = {
+    ["Start_Farm"] = true,
+    ["Enabled_HOP"] = false,
+    ["Enabled_Invisible"] = true,
+    ["Enabled_FastFarm"] = true,
+    ["Amount_FPS"] = 12,
+    ["LOCK_FPS"] = true,
+    ["WhiteScreen"] = false,
+    ["Disabled_Gui"] = true,
+}
+_G.Quests_Settings = {
+     ["V2Observation"] = true,
+     ["V2Aramament"] = true,
+     ["V2DarkLeg"] = true,
+     ["FarmRaid"] = true
+}
+getgenv().Key = "MARU-VO2N5-9LRH-6RFL-D0DCZ-8H4RP"
+getgenv().id = "759079095438278687"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MasterPClient/main/Private.lua"))()
